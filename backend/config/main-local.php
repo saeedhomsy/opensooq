5297382fa8c4123abcff21770b2c7ca1b284b2c3
saeedhomsy <homsysaeed@gmail.com>
2017-04-10 21:20:07 +0300

    for online9

---
+++ backend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'U9jbHxs0OBVV4PgP8SuOsf-mnWETVozC',
+        ],
+    ],
+];
---
+++ frontend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'PNGPmvkd0lI5cACDN87Sy7sdGZ0GKWvx',
+        ],
+    ],
+];
